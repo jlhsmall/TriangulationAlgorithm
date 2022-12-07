7c32696beebf7cc0984aea04d818f0c52d58d757
jlhsmall <519323064@qq.com>
2022-12-07 13:30:33 +0800

    seemingly finished

--- src/Triangulation.java
+++ src/Triangulation.java
@@ -3,18 +3,20 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Triangulation {
-    int cnt = 0;
-
+    public int cnt=0;
     public class Variable {
         byte val;
         int id;
         boolean isProcessed;
         LinkedHashSet<Equation> NonProcessedEquation = new LinkedHashSet<>();
-
-        public Variable(byte val, int id) {
+        public Variable(byte val) {
             this.val = val;
-            this.id = id;
+            this.id = ++cnt;
             isProcessed = false;
+        }
+        @Override
+        public int hashCode() {
+            return id;
         }
     }
 
@@ -47,12 +49,18 @@ public class Triangulation {
     public Set<Variable> getFreeVariables() {
         while(true){
             Variable variable=null;
+            //the searching can be optimized by a multimap, but I'm just lazy.
             for (Variable v : AllVariables){
                 if (!v.isProcessed && v.NonProcessedEquation.size()==1){
                     variable=v;break;
                 }
             }
             if(variable==null)break;
+            Equation e=variable.NonProcessedEquation.iterator().next();
+            e.isProcessed=true;
+            for(Variable v : e.Variables){
+                v.NonProcessedEquation.remove(e);
+            }
         }
         LinkedHashSet<Variable>FreeVariables=new LinkedHashSet<>();
         for(Variable v : AllVariables){
